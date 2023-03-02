e5710258432f707a6fbe9d24e267630b22f61c9d
Anold Kupara <kuparaanold20@gmail.com>
2023-03-02 13:27:48 +0200

    configured the dbcontext in Program.cs

--- FinancesTracker/Entities/Balance.cs
+++ FinancesTracker/Entities/Balance.cs
@@ -2,5 +2,12 @@
 {
     public class Balance
     {
+        public int BalanceId { get; set; }
+        public decimal TotalIncome { get; set; }
+        public decimal TotalExpense { get; set; }
+        public decimal BalanceAmount { get { return TotalIncome - TotalExpense; } }
+
+        public ICollection<Income> Incomes { get; set; }
+        public ICollection<Expense> Expenses { get; set; }
     }
 }
