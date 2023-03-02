57d155a1724efefe60597ad7ca99c93dbf1478c4
Anold Kupara <kuparaanold20@gmail.com>
2023-03-02 13:52:26 +0200

    added migration initailMigration1

---
+++ FinancesTracker/Migrations/20230302114945_initailMigration1.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinancesTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class initailMigration1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Balances_BalanceId",
+                table: "Expenses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Incomes_Balances_BalanceId",
+                table: "Incomes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BalanceId",
+                table: "Incomes",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BalanceId",
+                table: "Expenses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Balances_BalanceId",
+                table: "Expenses",
+                column: "BalanceId",
+                principalTable: "Balances",
+                principalColumn: "BalanceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Incomes_Balances_BalanceId",
+                table: "Incomes",
+                column: "BalanceId",
+                principalTable: "Balances",
+                principalColumn: "BalanceId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Balances_BalanceId",
+                table: "Expenses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Incomes_Balances_BalanceId",
+                table: "Incomes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BalanceId",
+                table: "Incomes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BalanceId",
+                table: "Expenses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Balances_BalanceId",
+                table: "Expenses",
+                column: "BalanceId",
+                principalTable: "Balances",
+                principalColumn: "BalanceId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Incomes_Balances_BalanceId",
+                table: "Incomes",
+                column: "BalanceId",
+                principalTable: "Balances",
+                principalColumn: "BalanceId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
