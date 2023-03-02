8be22e09b9833aec49e2707b59b31021409be03f
Anold Kupara <kuparaanold20@gmail.com>
2023-03-02 12:58:09 +0200

    added income model class

---
+++ FinancesTracker/Entities/Income.cs
@@ -0,0 +1,13 @@
+﻿namespace FinancesTracker.Entities
+{
+    public class Income
+    {
+        public int IncomeId { get; set; }
+        public decimal Amount { get; set; }
+        public string Source { get; set; }
+        public DateTime DateReceived { get; set; }
+
+        public int BalanceId { get; set; }
+        public Balance Balance { get; set; }
+    }
+}
