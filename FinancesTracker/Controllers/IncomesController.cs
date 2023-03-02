4d3f702467144ef73450e56954f7d2a21da9a91b
Anold Kupara <kuparaanold20@gmail.com>
2023-03-02 14:00:29 +0200

    Added teh IncomesController

---
+++ FinancesTracker/Controllers/IncomesController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FinancesTracker.DbContexts;
+using FinancesTracker.Entities;
+
+namespace FinancesTracker.Controllers
+{
+    public class IncomesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IncomesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Incomes
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Incomes.ToListAsync());
+        }
+
+        // GET: Incomes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Incomes == null)
+            {
+                return NotFound();
+            }
+
+            var income = await _context.Incomes
+                .FirstOrDefaultAsync(m => m.IncomeId == id);
+            if (income == null)
+            {
+                return NotFound();
+            }
+
+            return View(income);
+        }
+
+        // GET: Incomes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Incomes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IncomeId,Amount,Source,DateReceived")] Income income)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(income);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(income);
+        }
+
+        // GET: Incomes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Incomes == null)
+            {
+                return NotFound();
+            }
+
+            var income = await _context.Incomes.FindAsync(id);
+            if (income == null)
+            {
+                return NotFound();
+            }
+            return View(income);
+        }
+
+        // POST: Incomes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IncomeId,Amount,Source,DateReceived")] Income income)
+        {
+            if (id != income.IncomeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(income);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!IncomeExists(income.IncomeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(income);
+        }
+
+        // GET: Incomes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Incomes == null)
+            {
+                return NotFound();
+            }
+
+            var income = await _context.Incomes
+                .FirstOrDefaultAsync(m => m.IncomeId == id);
+            if (income == null)
+            {
+                return NotFound();
+            }
+
+            return View(income);
+        }
+
+        // POST: Incomes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Incomes == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Incomes'  is null.");
+            }
+            var income = await _context.Incomes.FindAsync(id);
+            if (income != null)
+            {
+                _context.Incomes.Remove(income);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool IncomeExists(int id)
+        {
+          return _context.Incomes.Any(e => e.IncomeId == id);
+        }
+    }
+}
