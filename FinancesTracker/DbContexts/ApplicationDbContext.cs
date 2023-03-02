62c80e5da10dd21b91ba64ec163b4cab82702d0a
Anold Kupara <kuparaanold20@gmail.com>
2023-03-02 13:18:21 +0200

    added the ApplicationDbContext class

---
+++ FinancesTracker/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FinancesTracker.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinancesTracker.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Balance> Balances { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
